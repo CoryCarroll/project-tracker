5aa7899e7c0eafa17c196eab9212df96113cdfc0
CorysPC <corystory2@gmail.com>
2022-07-16 13:35:55 -0600

    cleaned up html file

--- script.js
+++ script.js
@@ -7,34 +7,21 @@ getDate();
 
 // Creating save button icon
 var saveBtn = $('.saveBtn');
-var saveButton = $("<i class ='fas fa-save fa-lg'></i>")
-saveBtn.append(saveButton);
 
 // Create click event for save button
-$("saveBtn").on('click', function(event) {
+$("#saveBtn").on('click', function(event) {
     event.preventDefault();
-    var saveIndex = $(this).siblings(".description").children(".future").attr("id");
-    saveIndex.reminder = $(this).siblings(".description").childern(".future").val();
-    console.log(saveIndex);
-    saveReminders();
-    showReminders();
 })
 
 // Creates past present future classes depending on time of day
-var plannerData = $("#reminder");
-plannerData.attr("id", currentHour.id);
-if (currentHour.time < moment().format("HH")) {
-    plannerData.attr ({
-        "class": "past",
-    })
-    } else if (currentHour.time === moment().format("HH")) {
-        plannerData.attr ({
-            "class": "present"
-        })
-    } else if (currentHour.time > moment().format("HH")) {
-        plannerData.attr ({
-            "class": "future"
-        })
+var plannerData = $("#hour-9");
+var currentHour = moment().format('hh');
+    if (currentHour < moment().format("hh")) {
+    plannerData.addClass('past');
+    } else if (currentHour.time === moment().format("hh")) {
+        plannerData.addClass('present');
+    } else if (currentHour.time > moment().format("hh")) {
+        plannerData.addClass('future');
     };
 
 
